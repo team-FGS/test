1048d037ccd25c261d6735b10a83da9ccc007829
sunninety1 <45270166+sunninety1@users.noreply.github.com>
2018-12-13 23:27:33 +0700

    Add files via upload

--- 13-12-18/ver3/Hello.py
+++ 13-12-18/ver3/Hello.py
@@ -16,31 +16,33 @@ def decodeCmd( cmd):
     dec1 =  int(cmd[4:6],16)
     value2 = int(cmd[6:8],16)
     dec2 = int(cmd[8:],16)
-    param_2 = ''
-    
+    temperature = ''
+    channel = ''
+    sound_volume = ''
+    boiling_status = ''
+    level = ''
+
     dev = db_session.query(db.devices).get(int(device))
-    
-    
+
+
     if device == '1':
         # 1 = Fan
         if _status == '1':
-            dev.param_1 = 'Level: ' + str(value1)
             dev.status = 'ON'
+            dev.level = str(value1)
         else:
-            dev.param_1 = '0'
             dev.status = 'OFF'
-        
+            dev.level = '0'        
         db_session.commit()
 
     elif device == '2':
-        # 2 = Airconditioner
+        # 2 = Airconditioner 
         if _status == '1':
-            dev.param_1 = str(value1) + '.' + str(dec1) + ' degrees celsius'
             dev.status = 'ON'
+            dev.temperature = str(value1) + '.' + str(dec1)
         else:
-            dev.param_1 = 'undetected'
             dev.status = 'OFF'
-        
+            dev.temperature = 'undetected'
         db_session.commit()
 
     elif device == '3':
@@ -48,44 +50,41 @@ def decodeCmd( cmd):
         print(3)
         if _status == '1':
             dev.status = 'ON'
-            dev.param_2 = str(value2) + '.' + str(dec2) + ' degrees celsius'
+            dev.temperature = str(value2) + '.' + str(dec2)
             if value1 == '1':
-                dev.param_1 = 'Boiled'
+                dev.boiling_status = 'Boiled'
             else:
-                dev.param_1 = 'Warming'
+                dev.boiling_status = 'Warming'
         else:
             dev.status = 'OFF'
-            dev.param_1 = 'OFF'
-            dev.param_2 = 'undetected'
-
+            dev.temperature = 'undetected'
         db_session.commit()
 
 
     elif device == '4':
         # 4 = Television
         if _status == '1':
-            dev.status = 'ON'
-            dev.param_1 = 'Channel: ' + str(value1)
-            dev.param_2 = 'Sound Volume: ' + str(value2)
+            dev.status = 'ON')
+            dev.channel = str(value1)
+            dev.sound_volume = str(value2)
         else:
             dev.status = 'OFF'
-            dev.param_1 = 'OFF'
-            dev.param_2 = 'OFF'
-
+            dev.channel = 'OFF'
+            dev.sound_volume = 'OFF'
+        
         db_session.commit()
 
     elif device == '5':
         # 5 = Radio
         if status == '1':
             dev.status = 'ON'
-            dev.param_1 = str(value1) + '.' + str(dec1)
-            dev.param_2 = str(value2)
+            dev.channel = str(value1) + '.' = str(dec1)
+            dev.sound_volume = str(value2)
         else:
             dev.status = 'OFF'
-            dev.param_1 = 'OFF'
-            dev.param_2 = 'OFF'
+            dev.channel = 'OFF'
+            dev.sound_volume = 'OFF'
         db_session.commit()
-
 
 @app.route('/')
 @app.route('/main')
